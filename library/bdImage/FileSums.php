14316a64d33b2cdc0013bf4c5a650324930ad573
Dao Hoang Son <daohoangson@gmail.com>
2013-03-05 21:48:36 +0700

    v0.9-dev5 -- version bump

--- library/bdImage/FileSums.php
+++ library/bdImage/FileSums.php
@@ -6,14 +6,17 @@ class bdImage_FileSums
 	{
 		return array (
   'library/bdImage/BbCode/Formatter/Collector.php' => '8e1a51df8109ee145fa7bd8ba9ab2d4f',
-  'library/bdImage/Installer.php' => 'c402277cbe9bf74b48972b5a36ce6fea',
+  'library/bdImage/Installer.php' => '49262225f5f59eb52f82edfb3497e268',
   'library/bdImage/Integration.php' => '52c6dffd0327583e6edf2c560c09e99a',
-  'library/bdImage/Listener.php' => '8147508f7626198538d44d27c03a44df',
+  'library/bdImage/Listener.php' => 'a7fa9197af03942fa5e63d7ad9c5e0d9',
   'library/bdImage/ThirdParties/Fastimage.php' => '1eadd44686ad479e4932c5072dc3a8bc',
   'library/bdImage/thumbnail.php' => '1dc05d206b893e0f3866666df980beae',
-  'library/bdImage/XenForo/DataWriter/Discussion/Thread.php' => '55af3b3cd85cb13934d98b2d3c9e20d7',
-  'library/bdImage/XenForo/DataWriter/DiscussionMessage/Post.php' => 'f030a54fc5c4eb871519712d1126a7d8',
+  'library/bdImage/XenForo/DataWriter/Discussion/Thread.php' => 'efae01ba9cb2b9dfd7cf43369df2dc33',
+  'library/bdImage/XenForo/DataWriter/DiscussionMessage/Post.php' => 'b006082a08c4a1de42fca584c2ce275b',
+  'library/bdImage/XenForo/DataWriter/Forum.php' => 'a0424731e0b110fee4b29eb0fb7ae770',
   'library/bdImage/XenForo/Model/Log.php' => '012d722d33eec6b32705df89eefad618',
+  'library/bdImage/XenForo/Model/Post.php' => '31490f39906a87bce61a3ee8411fa94e',
+  'library/bdImage/XenForo/Model/Thread.php' => 'a69ccdd5e7f91d0664ed6eb327901b7d',
 );
 	}
 }

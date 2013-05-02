c0bc83880487364e952f7ba5aa69409ef4804043
Dao Hoang Son <daohoangson@gmail.com>
2013-05-02 09:22:14 +0700

    v0.9-dev10 -- version bump

--- library/bdImage/FileSums.php
+++ library/bdImage/FileSums.php
@@ -5,13 +5,13 @@ class bdImage_FileSums
 	public static function getHashes()
 	{
 		return array (
-  'library/bdImage/BbCode/Formatter/Collector.php' => '8e1a51df8109ee145fa7bd8ba9ab2d4f',
+  'library/bdImage/BbCode/Formatter/Collector.php' => '9e1e2f6eeb8542d7865ff52c6ac36c2b',
   'library/bdImage/Injection/WidgetFramework/WidgetRenderer/Threads.php' => 'd908a54e81178c9f1005b5d4e4183978',
   'library/bdImage/Installer.php' => '49262225f5f59eb52f82edfb3497e268',
-  'library/bdImage/Integration.php' => '89c4c78717446dfcb107ccd5f99239b7',
-  'library/bdImage/Listener.php' => '8ec13c833bd4197d33440fc8f5eef8b4',
+  'library/bdImage/Integration.php' => 'fd667b884a32f770cea55ffa51866ecc',
+  'library/bdImage/Listener.php' => 'a3ac906f22932b8db0a62791b3c62b9a',
   'library/bdImage/ThirdParties/Fastimage.php' => 'be326f919949dfc594725b3b9d154930',
-  'library/bdImage/thumbnail.php' => '970b6b5f2b27bc7d6c5f06bf1a1d6d81',
+  'library/bdImage/thumbnail.php' => '17015be18f2cbe18c0f2600f4434169f',
   'library/bdImage/XenForo/ControllerPublic/Attachment.php' => '025d7264955a3f7bf6459a9bb8678578',
   'library/bdImage/XenForo/DataWriter/Discussion/Thread.php' => 'efae01ba9cb2b9dfd7cf43369df2dc33',
   'library/bdImage/XenForo/DataWriter/DiscussionMessage/Post.php' => 'b006082a08c4a1de42fca584c2ce275b',

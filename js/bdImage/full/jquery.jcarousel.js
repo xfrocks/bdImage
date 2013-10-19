70ace7db50e83caa9da2b0fe09007b7035245085
Dao Hoang Son <daohoangson@gmail.com>
2013-10-19 16:17:08 +0700

    added arrows

--- js/bdImage/full/jquery.jcarousel.js
+++ js/bdImage/full/jquery.jcarousel.js
@@ -175,9 +175,9 @@
         this.buttonNext.css('display', 'block');
         this.buttonPrev.css('display', 'block');
 
-        this.funcNext   = function() { self.next(); };
-        this.funcPrev   = function() { self.prev(); };
-        this.funcResize = function() { 
+        this.funcNext   = function() { self.next(); return false; };
+        this.funcPrev   = function() { self.prev(); return false; };
+        this.funcResize = function() {
             if (self.resizeTimer) {
                 clearTimeout(self.resizeTimer);
             }
@@ -191,7 +191,7 @@
             this.options.initCallback(this, 'init');
         }
 
-        if (!windowLoaded && $.browser.safari) {
+        if (!windowLoaded && $jc.isSafari()) {
             this.buttons(false, false);
             $(window).bind('load.jcarousel', function() { self.setup(); });
         } else {
@@ -203,7 +203,7 @@
     var $jc = $.jcarousel;
 
     $jc.fn = $jc.prototype = {
-        jcarousel: '0.2.8'
+        jcarousel: '0.2.9'
     };
 
     $jc.fn.extend = $jc.extend = $.extend;
@@ -961,8 +961,16 @@
 
             if (d == null) {
                 return !this.options.vertical ?
-                       (el.outerWidth(true) || $jc.intval(this.options.itemFallbackDimension)) :
-                       (el.outerHeight(true) || $jc.intval(this.options.itemFallbackDimension));
+                    ((el.innerWidth() +
+                        $jc.intval(el.css('margin-left')) +
+                        $jc.intval(el.css('margin-right')) +
+                        $jc.intval(el.css('border-left-width')) +
+                        $jc.intval(el.css('border-right-width'))) || $jc.intval(this.options.itemFallbackDimension)) :
+                    ((el.innerHeight() +
+                        $jc.intval(el.css('margin-top')) +
+                        $jc.intval(el.css('margin-bottom')) +
+                        $jc.intval(el.css('border-top-width')) +
+                        $jc.intval(el.css('border-bottom-width'))) || $jc.intval(this.options.itemFallbackDimension));
             } else {
                 var w = !this.options.vertical ?
                     d - $jc.intval(el.css('marginLeft')) - $jc.intval(el.css('marginRight')) :
@@ -1008,6 +1016,14 @@
 
         windowLoaded: function() {
             windowLoaded = true;
+        },
+
+        isSafari: function() {
+            var ua = navigator.userAgent.toLowerCase(),
+                match = /(chrome)[ \/]([\w.]+)/.exec(ua) || /(webkit)[ \/]([\w.]+)/.exec(ua) || [],
+                browser = match[1] || "";
+
+            return browser === 'webkit';
         }
     });
 
--- js/bdImage/full/widget_slider.js
+++ js/bdImage/full/widget_slider.js
@@ -1,21 +1,26 @@
-/** @param {jQuery} $ jQuery Object */
-!function($, window, document, _undefined)
+/** @param {jQuery} $ jQuery Object */! function($, window, document, _undefined)
 {
-	XenForo.bdImage_Widget_Slider_Container = function($container) { this.__construct($container); };
+	XenForo.bdImage_Widget_Slider_Container = function($container)
+	{
+		this.__construct($container);
+	};
 	XenForo.bdImage_Widget_Slider_Container.prototype =
 	{
 		__construct: function($container)
 		{
-			$container.find('ul').jcarousel({
+			$container.find('ul').jcarousel(
+			{
 				animation: 'slow',
 				auto: 3,
 				scroll: 1,
 				wrap: 'circular',
 				initCallback: function(carousel)
 				{
-					carousel.clip.hover(function() {
+					carousel.clip.hover(function()
+					{
 						carousel.stopAuto();
-					}, function() {
+					}, function()
+					{
 						carousel.startAuto();
 					});
 				}
@@ -27,5 +32,4 @@
 
 	XenForo.register('.bdImage_Widget_Slider_Container', 'XenForo.bdImage_Widget_Slider_Container');
 
-}
-(jQuery, this, document);
+}(jQuery, this, document); 

ef51586aace8ae410f452238f5c497e13c9ffa82
Dao Hoang Son <daohoangson@gmail.com>
2018-02-11 21:37:33 +0700

    Improve api thread data

--- library/bdImage/bdApi/Extend/Model/Thread.php
+++ library/bdImage/bdApi/Extend/Model/Thread.php
@@ -7,28 +7,39 @@ class bdImage_bdApi_Extend_Model_Thread extends XFCP_bdImage_bdApi_Extend_Model_
         $data = call_user_func(array('parent', 'prepareApiDataForThread'), $thread, $forum, $firstPost);
 
         $imageData = bdImage_Helper_Template::getImageData('', $thread);
-        if (!empty($imageData)) {
-            $unpacked = bdImage_Helper_Data::unpack($imageData);
+        if (empty($imageData)) {
+            return $data;
+        }
 
-            $thumbnailSize = intval(XenForo_Application::getOptions()->get('attachmentThumbnailDimensions'));
-            if ($thumbnailSize > 0) {
-                $data['thread_thumbnail'] = array(
-                    'link' => new bdImage_Helper_LazyThumbnailer($unpacked, $thumbnailSize),
-                    'width' => $thumbnailSize,
-                    'height' => $thumbnailSize,
-                );
-            }
+        $unpacked = bdImage_Helper_Data::unpack($imageData);
+        $imageUrl = bdImage_Integration::getOriginalUrl($unpacked);
+        if (empty($imageUrl)) {
+            return $data;
+        }
 
-            list($width, $height) = bdImage_Integration::getSize($unpacked, false);
-            $data['thread_image'] = array(
-                'link' => bdImage_Integration::getOriginalUrl($unpacked),
-                'width' => intval($width),
-                'height' => intval($height),
+        $thumbnailSize = intval(XenForo_Application::getOptions()->get('attachmentThumbnailDimensions'));
+        if ($thumbnailSize > 0) {
+            $data['thread_thumbnail'] = array(
+                'link' => new bdImage_Helper_LazyThumbnailer($unpacked, $thumbnailSize),
+                'width' => $thumbnailSize,
+                'height' => $thumbnailSize,
             );
+        }
 
-            if (!empty($unpacked['is_cover'])) {
-                $data['thread_image']['display_mode'] = 'cover';
-            }
+        list($width, $height) = bdImage_Integration::getSize($unpacked, false);
+        $data['thread_image'] = array(
+            'link' => $imageUrl,
+            'width' => intval($width),
+            'height' => intval($height),
+        );
+
+        if (!empty($unpacked['is_cover'])) {
+            $data['thread_image']['display_mode'] = 'cover';
+        }
+
+        if (empty($data['links']['image'])) {
+            // legacy support
+            $data['links']['image'] = $data['thread_image']['link'];
         }
 
         return $data;

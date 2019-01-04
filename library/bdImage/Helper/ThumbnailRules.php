261b138735f076e54c96f06a335b5e293cc86251
Dao Hoang Son <daohoangson@gmail.com>
2019-01-04 14:32:52 +0700

    Fix safe image url detection for imgproxy

--- library/bdImage/Helper/ThumbnailRules.php
+++ library/bdImage/Helper/ThumbnailRules.php
@@ -118,7 +118,9 @@ class bdImage_Helper_ThumbnailRules
 
     public static function imgproxy($root, $key, $salt, $imageUrl, $options)
     {
-        if (strpos($imageUrl, '?') === false) {
+        # https://tools.ietf.org/html/rfc3986#section-2.2: gen-delims
+        # https://tools.ietf.org/html/rfc3986#section-2.3: unreserved
+        if (preg_match('#^[:/\[\]@A-Za-z0-9\-\._~]+$#', $imageUrl)) {
             $path = "/$options/plain/$imageUrl";
         } else {
             $imageUrlB64 = self::_imgproxyBase64($imageUrl);

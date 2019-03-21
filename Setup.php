14ff1130dfdb9d0f3a90ea331280ead34041f349
Luu Truong <truonglv@outlook.com>
2019-03-21 15:24:39 +0700

    Initial for XF2

---
+++ Setup.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Xfrocks\Image;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+}

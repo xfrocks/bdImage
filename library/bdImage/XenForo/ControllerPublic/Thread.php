725ef7d72d55705f240283ece7fb3a54e5cdbbb1
Luu Truong <truonglv@outlook.com>
2020-06-24 09:43:19 +0700

    Check permission canEditThread

--- library/bdImage/XenForo/ControllerPublic/Thread.php
+++ library/bdImage/XenForo/ControllerPublic/Thread.php
@@ -30,6 +30,10 @@ class bdImage_XenForo_ControllerPublic_Thread extends XFCP_bdImage_XenForo_Contr
         $ftpHelper = $this->getHelper('ForumThreadPost');
         list($thread, $forum) = $ftpHelper->assertThreadValidAndViewable($threadId);
 
+        if (!$this->_getThreadModel()->canEditThread($thread, $forum, $errorPhraseKey)) {
+            throw $this->getErrorOrNoPermissionResponseException($errorPhraseKey);
+        }
+
         $post = $ftpHelper->getPostOrError($thread['first_post_id']);
         $postModel = $this->_getPostModel();
         if (!$postModel->canViewPost($post, $thread, $forum, $errorPhraseKey)) {

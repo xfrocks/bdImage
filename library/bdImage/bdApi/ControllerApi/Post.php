393333bced34bdcb88ccb7ff27380e9abc5c17ca
Le Thanh Tan <thanhtan1105@gmail.com>
2020-07-13 16:40:05 +0700

    add key image_is_thread_image and image_is_cover to thread (#17)

--- library/bdImage/bdApi/ControllerApi/Post.php
+++ library/bdImage/bdApi/ControllerApi/Post.php
@@ -8,17 +8,23 @@ class bdImage_bdApi_ControllerApi_Post extends XFCP_bdImage_bdApi_ControllerApi_
 
         if ($this->_isFieldIncluded('post_images') && ($response instanceof XenForo_ControllerResponse_View)) {
             $input = $this->_input->filter(array('post_id' => XenForo_Input::UINT));
-            list($post,,) = $this->_getForumThreadPostHelper()->assertPostValidAndViewable($input['post_id']);
+            list($post, $thread,) = $this->_getForumThreadPostHelper()->assertPostValidAndViewable($input['post_id']);
 
             /** @var bdImage_XenForo_DataWriter_DiscussionMessage_Post $firstPostDw */
             $firstPostDw = XenForo_DataWriter::create('XenForo_DataWriter_DiscussionMessage_Post');
             $firstPostDw->setExistingData($post, true);
 
+            $threadImageData = bdImage_Helper_Data::unpack($thread['bdimage_image']);
             $postBodyImages = array();
             $imageDataList = $firstPostDw->bdImage_extractImage(true);
             foreach ($imageDataList as $imageData) {
                 $data = bdImage_Helper_Data::unpack($imageData);
-                $postBodyImages[] = array('image_url' => $data[bdImage_Helper_Data::IMAGE_URL], 'data' => $imageData);
+                $postBodyImages[] = array(
+                    'image_url' => $data[bdImage_Helper_Data::IMAGE_URL],
+                    'data' => $imageData,
+                    'image_is_thread_image' => $data[bdImage_Helper_Data::IMAGE_URL] === $threadImageData[bdImage_Helper_Data::IMAGE_URL] ? true : false,
+                    'image_is_cover' => ($data[bdImage_Helper_Data::IMAGE_URL] === $threadImageData[bdImage_Helper_Data::IMAGE_URL] && $threadImageData['is_cover']) ? true : false
+                );
             }
             $response->params['post_images'] = $postBodyImages;
         }
